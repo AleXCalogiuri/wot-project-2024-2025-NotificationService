494dcab7366b8e4dc97fed0decf4adc2dd990774
Alessio Calogiuri <calogiuri.alessio@yahoo.com>
2025-10-23 12:05:32 +0200

    fix su docker e rabbit mq

--- src/main/java/com/unisalento/wotproject20242025notificationservice/restcontrollers/NotificationRestController.java
+++ src/main/java/com/unisalento/wotproject20242025notificationservice/restcontrollers/NotificationRestController.java
@@ -1,14 +1,18 @@
 package com.unisalento.wotproject20242025notificationservice.restcontrollers;
 
+import com.unisalento.wotproject20242025notificationservice.domain.Notification;
+import com.unisalento.wotproject20242025notificationservice.domain.NotificationUser;
 import com.unisalento.wotproject20242025notificationservice.dto.NotificationDTO;
 import com.unisalento.wotproject20242025notificationservice.repositories.NotificationAdminRepository;
 import com.unisalento.wotproject20242025notificationservice.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/getNotification")
@@ -35,4 +39,19 @@ public class NotificationRestController {
         return notificationService.deleteNotify(notificationDTO);
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public void setAdminIsLetta(@PathVariable String id) {
+        notificationService.setAdminIsLetta(id);
+    }
+
+    @RequestMapping(value = "/personal/{id}", method = RequestMethod.GET)
+    public void setUserIsLetta(@PathVariable String id) {
+        notificationService.setUserIsLetta(id);
+    }
+
+
+
+
+
 }
+
